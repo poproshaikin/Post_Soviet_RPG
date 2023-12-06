86ae903fa8e0db1a0a9ce48b382ba4f34bd03d99
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2023-12-06 20:56:44 +0100

    test

---
+++ Post_Soviet_RPG/Post_Soviet_RPG/Location.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Post_Soviet_RPG
+{
+    public abstract class Location
+    {
+        public abstract string Name { get; set; }
+        public abstract List<Location> NeighboringLocations { get; set; }
+        public abstract bool IsMainCharacterHere { get; set; }        
+    }
+}
